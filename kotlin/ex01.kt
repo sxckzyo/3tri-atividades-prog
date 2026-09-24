32f503fcf466f31d364bfaeda5c239687a90137f
Rafael Kiss Fortunato <rafael.kiss.fortunato@escola.pr.gov.br>
2026-09-24 11:33:27 -0300

    Update and rename exo1.kt to ex01.kt

---
+++ kotlin/ex01.kt
@@ -0,0 +1,8 @@
+fun main() {
+  println("======================================")
+  println("MEU PRIMEIRO PROGRAMA")
+  println("======================================")
+  println("Nome: Aluno Exemplo")
+  println("Turma: 3DS")
+  println("Escola: Alberto Gomes Veiga")
+  println("Bem-vindo ao Kotlin!")
