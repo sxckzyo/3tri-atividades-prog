bdb24468ea3b5ec1c79765fea82d735fe9e1f2d3
Rafael Kiss Fortunato <rafael.kiss.fortunato@escola.pr.gov.br>
2026-09-24 11:36:10 -0300

    Create ex02.kt

---
+++ kotlin/ex02.kt
@@ -0,0 +1,12 @@
+fun main() {
+  val nome = "Ana"
+  val idade = 17
+  val cidade = "Paranaguá"
+  val linguagemFavorita = "Python"
+
+  println("APRESENTAÇÃO PESSOAL")
+  println("Nome: $nome")
+  println("Idade: $idade anos")
+  println("Cidade: $cidade")
+  println("Linguagem favorita: $linguagemFavorita")
+}
